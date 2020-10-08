0f8c2f9fdb5679dd94177c887f020f2cdb25c788
ahmad@karigar.pk <ahmad@karigar.pk>
2020-10-08 17:22:11 +0500

    fixess

--- src/pages/company/Add.js
+++ src/pages/company/Add.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import getData from '../../components/getData';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
 export default class Add extends Component {
 	state = {
 		company_name: '',
@@ -11,6 +13,16 @@ export default class Add extends Component {
 		email: '',
 		centerType: '',
 		Logo: ''
+	};
+	handleSelect = (e) => {
+		console.log(e.target);
+	};
+
+	handleChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+	handleFile = (e) => {
+		console.log(e.target.files[0]);
 	};
 	render() {
 		return (
@@ -101,7 +113,61 @@ export default class Add extends Component {
 				</Row>
 				<Row form>
 					<Col md={12}>
-						<h5>Gender</h5>
+						<h5>Center Name</h5>
+					</Col>
+					<Col md={12}>
+						<FormGroup>
+							<Input
+								type="text"
+								name="centerName"
+								value={this.state.centerName}
+								onChange={(e) => this.handleChange(e)}
+							/>
+						</FormGroup>
+					</Col>
+					<Col md={12}>
+						<h5>Name</h5>
+					</Col>
+					<Col md={6}>
+						<FormGroup>
+							<Input
+								type="text"
+								name="firstName"
+								value={this.state.firstName}
+								onChange={(e) => this.handleChange(e)}
+							/>
+							<Label>First Name</Label>
+						</FormGroup>
+					</Col>
+					<Col md={6}>
+						<FormGroup>
+							<Input
+								type="text"
+								name="lastName"
+								value={this.state.lastName}
+								onChange={(e) => this.handleChange(e)}
+							/>
+							<Label>Last Name</Label>
+						</FormGroup>
+					</Col>
+				</Row>
+				<Row form>
+					<Col md={12}>
+						<h5>Phone</h5>
+					</Col>
+					<Col md={12}>
+						<FormGroup className={'customInputWidth'}>
+							<PhoneInput
+								country={'us'}
+								value={this.state.phone}
+								onChange={(phone) => this.setState({ phone })}
+							/>
+						</FormGroup>
+					</Col>
+				</Row>
+				<Row form>
+					<Col md={12}>
+						<h5>Centre Type</h5>
 					</Col>
 					<Col md={12}>
 						<FormGroup>
@@ -111,33 +177,14 @@ export default class Add extends Component {
 							</Input>
 						</FormGroup>
 					</Col>
-					<Col md={12}>
-						<h5>Date Of Birth</h5>
-					</Col>
-					<Col md={12}>
-						<FormGroup>
-							<Input
-								type="date"
-								name="DOB"
-								value={this.state.DOB}
-								onChange={(e) => this.handleChange(e)}
-							/>
-						</FormGroup>
-					</Col>
 				</Row>
 				<Row form>
 					<Col md={12}>
-						<h5>Notes</h5>
+						<h5>Centre Type</h5>
 					</Col>
 					<Col md={12}>
 						<FormGroup>
-							<Input
-								type="textarea"
-								name="notes"
-								onChange={(e) => {
-									this.handleChange(e);
-								}}
-							/>
+							<Input type="file" name="gender" onChange={(e) => this.handleFile(e)} />
 						</FormGroup>
 					</Col>
 				</Row>
